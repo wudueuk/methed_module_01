094275bb3a690c2c0c2c322ae1812412fba3e11f
wudueuk <wudueuk@gmail.com>
2021-12-17 10:50:00 +0200

    Создание первого коммита

---
+++ main.js
@@ -0,0 +1,8 @@
+const productName = "Холодильник";
+let productCount = 9;
+const productCategory = "Бытовая техника";
+let productCost = 33000;
+
+console.log('productName: ', productName);
+
+console.log(productCount * productCost);
