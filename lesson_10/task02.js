1dc21c635d60ff95d238c131e63ab6eb2a383503
wudueuk <wudueuk@gmail.com>
2022-01-14 13:32:32 +0200

    lesson_10

--- lesson_10/task02.js
+++ lesson_10/task02.js
@@ -1,11 +1,13 @@
 'use strict';
 
 import { listIPv4 } from './ipv4.js';
-console.log('Length listIPv4: ', listIPv4.length);
+
+const getLength = (arr) => {
+	const newArr = new Set([...arr.values()]);
+	return newArr.size;
+}
 
 
-const weakIp = new Set([...listIPv4.values()]);
 
-console.log('weakIp: ', weakIp);
-console.log('Size weakIp: ', weakIp.size);
+console.log('Количество уникальных ip-адресов - ', getLength(listIPv4));
 
